2726924dbea88cd0db26de6f412d4ce8d0417b5f
Ward Gauderis <ward.gauderis@vub.be>
2024-04-16 11:40:55 +0200

    temp sync

--- generate_datasets.py
+++ generate_datasets.py
@@ -45,8 +45,6 @@ def progression(positions: np.ndarray) -> bool:
 	length = len(position)
 	return (positions[0] + 1) % length == positions[1] and (positions[1] + 1) % length == positions[2]
 
-#%%
-
 def sample_color(correct: bool):
 	if correct:
 		return np.random.choice(list(color.keys()), 3, replace=False)
@@ -69,7 +67,7 @@ def sample_position(correct: bool):
 
 #%%
 
-def create_balanced_dataset(name: str, samples: int):
+def create_dataset(name: str, samples: int, type: str):
 	Path(name).mkdir(parents=True, exist_ok=True)
 
 	puzzles = pd.DataFrame(index=range(samples),
@@ -77,81 +75,40 @@ def create_balanced_dataset(name: str, samples: int):
 	color_relations = pd.DataFrame(index=range(samples * 3), columns=["correct"])
 	position_relations = pd.DataFrame(index=range(samples * 3), columns=["correct"])
 	images = pd.DataFrame(index=range(samples * 9), columns=["color", "position"])
+ 
 
 	for i in range(samples):
-		correct_puzzle = np.random.rand() < 0.5
+		correct_puzzle = np.random.rand() < 0.5 if type == "balanced" else True
 
-		puzzles.loc[i]["correct"] = correct_puzzle
+		match type:
+			case "balanced":
+				colors = np.stack([sample_color(correct_puzzle) for _ in range(3)], axis=0).flatten()
+				color_relations.loc[i * 3:(i + 1) * 3 - 1] = correct_puzzle
 
-		colors = np.stack([sample_color(correct_puzzle) for _ in range(3)], axis=0).flatten()
-		positions = np.stack([sample_position(correct_puzzle) for _ in range(3)], axis=1).flatten()
+				positions = np.stack([sample_position(correct_puzzle) for _ in range(3)], axis=1).flatten()
+				position_relations.loc[i * 3:(i + 1) * 3 - 1] = correct_puzzle
+			case "independent":
+				probability = 0.22
+				colors = []
+				for j in range(3):
+					color_correct = np.random.rand() >= probability
+		
+					colors.append(sample_color(color_correct))
+					color_relations.loc[i * 3 + j] = color_correct
+				colors = np.stack(colors, axis=0).flatten()
+    
+				positions = []
+				for j in range(3):
+					position_correct = np.random.rand() >= probability
+
+					positions.append(sample_position(position_correct))
+					position_relations.loc[i * 3 + j] = position_correct
+				positions = np.stack(positions, axis=1).flatten()
+
+		puzzles.loc[i][["color_" + str(j) for j in range(9)]] = colors
+		puzzles.loc[i][["position_" + str(j) for j in range(9)]] = positions
+		images.loc[i * 9:(i + 1) * 9 - 1] = np.stack([colors, positions], axis=1)
   
-		color_relations.loc[i * 3:(i + 1) * 3 - 1] = correct_puzzle
-		position_relations.loc[i * 3:(i + 1) * 3 - 1] = correct_puzzle
-
-
-		puzzles.loc[i][["color_" + str(j) for j in range(9)]] = colors
-		puzzles.loc[i][["position_" + str(j) for j in range(9)]] = positions
-		images.loc[i * 9:(i + 1) * 9 - 1] = np.stack([colors, positions], axis=1)
-
-		for j in range(9):
-			instance = distribution.Product([
-				color[colors[j]],
-				distribution.Continuous("scale", 0.1, 0.3),
-				position[positions[j]],
-				saturation,
-				brightness,
-			]).sample()
-			instance["shape"] = "circle"
-
-			sprite = Sprite(**instance)
-
-			image = renderer.render([sprite])
-			Image.fromarray(image).save(os.path.join(name, f"{i * 9 + j}.png"))
-
-	puzzles.to_csv(os.path.join(name, "blackbird.csv"), index=False)
-	color_relations.to_csv(os.path.join(name, "distribute_three.csv"), index=False)
-	position_relations.to_csv(os.path.join(name, "progression.csv"), index=False)
-	images.to_csv(os.path.join(name, "product_concepts.csv"), index=False)
-
-#%%
-	
-def create_independent_dataset(name: str, samples: int):
-	Path(name).mkdir(parents=True, exist_ok=True)
-
-	puzzles = pd.DataFrame(index=range(samples),
-						   columns=["correct"] + [f"{type}_{i}" for i in range(9) for type in
-												  ["color", "position"]])
-	color_relations = pd.DataFrame(index=range(samples * 3), columns=["correct"])
-	position_relations = pd.DataFrame(index=range(samples * 3), columns=["correct"])
-	images = pd.DataFrame(index=range(samples * 9), columns=["color", "position"])
-
-	probability = 0.22
-
-	for i in range(samples):
-		correct_puzzle = True
-
-		colors = []
-		for j in range(3):
-			color_correct = np.random.rand() >= probability
-   
-			colors.append(sample_color(color_correct))
-			color_relations.loc[i * 3 + j] = color_correct
-
-		positions = []
-		for j in range(3):
-			position_correct = np.random.rand() >= probability
-
-			positions.append(sample_position(position_correct))
-			position_relations.loc[i * 3 + j] = position_correct
-
-		colors = np.stack(colors, axis=0).flatten()
-		positions = np.stack(positions, axis=1).flatten()
-
-		puzzles.loc[i][["color_" + str(j) for j in range(9)]] = colors
-		puzzles.loc[i][["position_" + str(j) for j in range(9)]] = positions
-		images.loc[i * 9:(i + 1) * 9 - 1] = np.stack([colors, positions], axis=1)
-
 		puzzles.loc[i]["correct"] = correct_puzzle
 
 		for j in range(9):
@@ -176,6 +133,15 @@ def create_independent_dataset(name: str, samples: int):
 
 #%%
 
+def create_datasets(path: str):
+	create_dataset(path + "/train", 3000, "balanced")
+	create_dataset(path + "/val", 300, "balanced")
+	create_dataset(path + "/test", 300, "balanced")
+ 
+	create_dataset(path + "/train", 3000, "independent")
+	create_dataset(path + "/val", 300, "independent")
+	create_dataset(path + "/test", 300, "independent")
+ 
 def display_puzzle(path: str, i: int):
 	images = imread_collection([os.path.join(path, f"{i}.png") for i in range(i * 9, (i + 1) * 9)],
 							   conserve_memory=False)
@@ -186,17 +152,6 @@ def display_puzzle(path: str, i: int):
 			ax[i, j].axis("off")
 	# plt.savefig("graphs/" + name, bbox_inches="tight", pad_inches=0, dpi=300, transparent=True)
 	plt.show()
-
-
-def create_datasets(path: str):
-	create_balanced_dataset(path + "/train", 3000)
-	create_balanced_dataset(path + "/val", 300)
-	create_balanced_dataset(path + "/test", 300)
-
-	create_independent_dataset(path + "/train", 3000)
-	create_independent_dataset(path + "/val", 300)
-	create_independent_dataset(path + "/test", 300)
- 
  
 def inspect_dataset(path: str):
 	puzzles = pd.read_csv(path + "/blackbird.csv")
